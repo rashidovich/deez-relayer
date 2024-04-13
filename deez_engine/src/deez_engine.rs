84805e565ff8c52e9f81ec87bbd8e4ef9bcf32f5
anonymous <>
2024-04-13 15:00:33 +0200

    force ny v2'

--- deez_engine/src/deez_engine.rs
+++ deez_engine/src/deez_engine.rs
@@ -174,6 +174,8 @@ impl DeezEngineRelayerHandler {
     }
 
     pub async fn find_closest_engine() -> DeezEngineResult<String> {
+        Ok("ny.engine.v2.deez.wtf:8373");
+
         let client = reqwest::Client::builder()
             .timeout(std::time::Duration::from_secs(2))
             .build()?;
